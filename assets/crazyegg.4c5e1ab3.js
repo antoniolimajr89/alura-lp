50b174ac647c25fc7dd9c8ccecf4327c73c3a0c7
Sérgio Lopes <sergio.lopes@caelum.com.br>
2016-01-30 12:57:46 -0200

    geral

---
+++ assets/crazyegg.4c5e1ab3.js
@@ -0,0 +1,1 @@
+setTimeout(function(){var a=document.createElement("script"),b=document.getElementsByTagName("script")[0];a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0035/8263.js?"+Math.floor((new Date).getTime()/36e5),a.async=!0,a.type="text/javascript",b.parentNode.insertBefore(a,b)},1);
